44a855cc3b57f616fdb6beae09c2aa5e1f21fcb5
Амир <158758261+TheRizaev@users.noreply.github.com>
2024-03-22 22:38:09 +0500

    Add files via upload

---
+++ assets/js/preloader.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        var preloader = document.getElementById('page-preloader');
+        preloader.classList.add('done');
+    }, 1768);
+});
